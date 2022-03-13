bea78027182f1d84ccc40393d207c23181c95eea
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-13 19:51:56 +0530

    Time: 228 ms (8.47%), Space: 67.7 MB (54.67%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int maxtillnow=nums[0], maxans=nums[0], n=nums.size();
+        if(n==1){
+            return maxtillnow;
+        }
+       for(int i=1;i<n;i++){
+           maxtillnow+=nums[i];
+           if(nums[i]>maxtillnow){
+               maxtillnow=nums[i];
+           }
+           maxans=max(maxans,  maxtillnow);
+       }
+        
+        return maxans;
+    }
+};
