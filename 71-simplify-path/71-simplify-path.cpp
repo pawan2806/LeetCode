4fffd3d6a743b38086254c0ef220e13e861ef2be
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-15 00:24:53 +0530

    Time: 8 ms (62.74%), Space: 8.2 MB (75.89%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -35,45 +35,23 @@ class Solution
                 s.push(temp);
             }
             temp = "";
-            temp+="/";
+            temp += "/";
             vector<string> ans;
-            while(!s.empty()){
+            while (!s.empty())
+            {
                 ans.push_back(s.top());
                 s.pop();
             }
             reverse(ans.begin(), ans.end());
-            for(int i=0;i<ans.size();i++){
-                temp+=ans[i];
-                if(i!=ans.size()-1){
-                    temp+="/";
+            for (int i = 0; i < ans.size(); i++)
+            {
+                temp += ans[i];
+                if (i != ans.size() - 1)
+                {
+                    temp += "/";
                 }
             }
 
             return temp;
-
-           	//        	// get current part: without /
-           	//         string front;
-           	//         int i = 1;
-           	//         while(i < path.size() && path[i] != '/') front.push_back(path[i++]);
-
-           	//        	// check . ..
-           	//         if(front == "." || front.empty()) {}
-           	//         else if(front == "..") {
-           	//            	// popping back the directory
-           	//             while(!out.empty() && out.back() != '/') out.pop_back();
-           	//            	// popping back the redundent backslash
-           	//             while(!out.empty() && out.back() == '/') out.pop_back();
-           	//         } else {
-           	//            	// concatenate the front part of the remained input string with the output path
-           	//            	// join with backslash
-           	//             out = out + "/" + front;
-           	//         }
-           	//         return simplify(path.substr(i), out);
-           	//     }
-
-           	//     string simplifyPath(string path) {
-           	//         string out;
-           	//         simplify(path, out);
-           	//         return out.empty() ? "/" : out;
         }
 };
