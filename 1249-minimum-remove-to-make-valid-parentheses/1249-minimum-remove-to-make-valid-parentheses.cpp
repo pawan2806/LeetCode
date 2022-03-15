c020541753395edc7f400892b83104fc8503e46f
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-15 20:32:10 +0530

    Time: 256 ms (5.03%), Space: 43.4 MB (5.01%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,64 @@
+class Solution
+{
+    public:
+        string minRemoveToMakeValid(string s)
+        {
+            int n = s.length();
+            string ans = "";
+            stack<pair<char, int>> stk;
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] != '(' && s[i] != ')')
+                {
+                    continue;
+                }
+                else if (s[i] == '(')
+                {
+                    pair<char, int> pair1;
+                    pair1.first = '(';
+                    pair1.second = i;
+                    stk.push(pair1);
+                }
+                else
+                {
+                    pair<char, int> pair1;
+
+                    if (!stk.empty())
+                    {
+                        pair1 = stk.top();
+                        if (pair1.first == ')')
+                        {
+                            pair1.second = i;
+                            stk.push(pair1);
+                        }
+                        else
+                        {
+                            stk.pop();
+                        }
+                    }
+                    else
+                    {
+                        pair1.first = ')';
+                        pair1.second = i;
+                        stk.push(pair1);
+                    }
+                }
+            }
+            unordered_map<int,int> mp;
+            while(!stk.empty()){
+                pair<char, int> pair1 =stk.top();
+                mp[pair1.second]++;
+                stk.pop();
+                
+            }
+            for(int i=0;i<n;i++){
+                if(mp[i]){
+                    continue;
+                } else {
+                    ans+=s[i];
+                }
+            }
+            return ans;
+            
+        }
+};
