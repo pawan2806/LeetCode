674f430d4863e57762b2a03e99e75ab446042d68
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-20 01:32:03 +0530

    Time: 81 ms (6.83%), Space: 18.3 MB (5.53%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    static bool comp(vector<int> a, vector<int> b){
+        return a[1]<b[1];
+    }
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        sort(trips.begin(), trips.end(), comp);
+        int n=trips.size();
+        vector<pair<int,int>> drop;
+        for(int i=0;i<n;i++){
+            drop.push_back({trips[i][2], trips[i][0]});
+        }
+        sort(drop.begin(), drop.end());
+        int j=0;
+        int currcap=0;
+        for(int i=0;i<n;i++){
+            int from=trips[i][1];
+            
+            while(j<n && drop[j].first<=from){
+                currcap-=drop[j].second;
+                j++;
+            }
+            currcap+=trips[i][0];
+            cout<<trips[i][0]<<trips[i][1]<<trips[i][2]<<endl;
+            if(currcap>capacity){
+                return false;
+            }
+        }
+        return true;
+    }
+};
