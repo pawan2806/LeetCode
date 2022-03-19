ad664fe7fdbdccfa68455b4a5014bc5b22de3dbd
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-20 01:40:00 +0530

    Time: 24 ms (43.63%), Space: 10.4 MB (68.97%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -4,28 +4,41 @@ public:
         return a[1]<b[1];
     }
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(), trips.end(), comp);
         int n=trips.size();
-        vector<pair<int,int>> drop;
+        vector<int> inc(1001,0);
         for(int i=0;i<n;i++){
-            drop.push_back({trips[i][2], trips[i][0]});
+            inc[trips[i][1]]+=trips[i][0];
+            inc[trips[i][2]]-=trips[i][0];
         }
-        sort(drop.begin(), drop.end());
-        int j=0;
         int currcap=0;
-        for(int i=0;i<n;i++){
-            int from=trips[i][1];
-            
-            while(j<n && drop[j].first<=from){
-                currcap-=drop[j].second;
-                j++;
-            }
-            currcap+=trips[i][0];
-            cout<<trips[i][0]<<trips[i][1]<<trips[i][2]<<endl;
+        for(int i=0;i<1001;i++){
+            currcap+=inc[i];
             if(currcap>capacity){
                 return false;
             }
         }
+//         sort(trips.begin(), trips.end(), comp);
+//         int n=trips.size();
+//         vector<pair<int,int>> drop;
+//         for(int i=0;i<n;i++){
+//             drop.push_back({trips[i][2], trips[i][0]});
+//         }
+//         sort(drop.begin(), drop.end());
+//         int j=0;
+//         int currcap=0;
+//         for(int i=0;i<n;i++){
+//             int from=trips[i][1];
+            
+//             while(j<n && drop[j].first<=from){
+//                 currcap-=drop[j].second;
+//                 j++;
+//             }
+//             currcap+=trips[i][0];
+//             cout<<trips[i][0]<<trips[i][1]<<trips[i][2]<<endl;
+//             if(currcap>capacity){
+//                 return false;
+//             }
+//         }
         return true;
     }
 };
