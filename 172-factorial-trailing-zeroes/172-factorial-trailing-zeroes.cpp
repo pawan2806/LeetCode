590da4f257f34ddf6167f51534a3906338187295
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-21 03:03:11 +0530

    Time: 8 ms (11.18%), Space: 6.2 MB (5.41%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int trailingZeroes(int n) {
         int count=0,i=1;
-        while(n/pow(5,i)>0){
-            count+=n/pow(5,i++);
-        }
+        while(n/pow(5,i)>0) count+=n/pow(5,i++);
+        
         return count;
     }
 };
