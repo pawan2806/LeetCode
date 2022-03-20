a6884812f6ddcd30135f54e7364ad467c8a136ac
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-20 17:21:52 +0530

    Time: 211 ms (5.03%), Space: 75.3 MB (12.28%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -1,34 +1,44 @@
-class Solution {
-public:
-    int totalHammingDistance(vector<int>& nums) {
-        
-        int n=nums.size();
-        vector<vector<int>> ans(n);
-        for(int i=0;i<n;i++){
-            int x=nums[i];
-            vector<int> gg(32,0);
-            int j=0;
-            while(x>0){
-                gg[j]=x%2;
-                j++;
-                x=x/2;
-            }
-            ans[i]=gg;
-            
-        }
-        long long int count=0;
-        for(int i=0;i<32;i++){
-            long long int count0=0;
-            long long int count1=0;
-            for(int j=0;j<n;j++){
-                if(ans[j][i]){
-                    count1++;
-                } else {
-                    count0++;
+class Solution
+{
+    public:
+
+        int totalHammingDistance(vector<int> &nums)
+        {
+
+            int n = nums.size();
+            vector<vector < int>> ans(n);
+            for (int i = 0; i < n; i++)
+            {
+                int x = nums[i];
+                vector<int> gg(32, 0);
+                int j = 0;
+                while (x > 0)
+                {
+                    gg[j] = x % 2;
+                    j++;
+                    x = x / 2;
                 }
+                ans[i] = gg;
             }
-            count+=count1*count0;
+            long long int count = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                long long int count0 = 0;
+                long long int count1 = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    int x=nums[j];
+                    if (x & (1 << i))
+                    {
+                        count1++;
+                    }
+                    else
+                    {
+                        count0++;
+                    }
+                }
+                count += count1 * count0;
+            }
+            return (int) count;
         }
-        return (int)count;
-    }
 };
