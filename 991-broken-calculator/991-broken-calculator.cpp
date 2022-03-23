5997ab1e2bdb683c4c70b904d03eaa0756329527
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-23 23:59:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+        int brokenCalc(int x, int y)
+        {
+            if(x>=y){
+                return x-y;
+            }
+            if(y%2==0){
+                return 1+brokenCalc(x, y/2);
+            }
+           
+           
+
+            return 1+brokenCalc(x, y+1);
+        }
+};
