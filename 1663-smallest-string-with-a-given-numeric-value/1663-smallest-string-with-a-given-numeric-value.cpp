b6f9dcefea15e9e27ce515153b801f34aaf37160
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-03-27 02:16:55 +0530

    Time: 48 ms (66.99%), Space: 27 MB (48.50%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -3,9 +3,13 @@ public:
     string getSmallestString(int n, int k) {
         string ans;
         for(int i=0;i<n;i++){
-            ans+='a';
+            ans+='a'; 
             k--;
         }
+        // now we have a lexicographically smallest string of size n with score n;
+        // remaining score = k-n;
+        // we iterate from back of string and incrementing the score of the string. 
+        // Iterating from behind because we have to find smallest string.
         int gg=n-1;
         while(k>0){
             if(k>25){
@@ -14,6 +18,7 @@ public:
                 k-=25;
             } else {
                 ans[gg]=ans[gg]+k;
+                // updating remaining score to 0
                 k=0;
             }
         }
