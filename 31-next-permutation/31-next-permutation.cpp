0359a88cf012528ee551788eaab9110d61a138a3
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-04-03 17:43:58 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (75.54%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,26 +1,32 @@
-class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-        int i=nums.size()-1;
-        for(;i>0;i--){
-            if(nums[i-1]<nums[i]){
-                break;
+class Solution
+{
+    public:
+        void nextPermutation(vector<int> &nums)
+        {
+            int i = nums.size() - 1;
+            for (; i > 0; i--)
+            {
+                if (nums[i - 1] < nums[i])
+                {
+                    break;
+                }
             }
-        }
-        if(i==0){
-            sort(nums.begin(), nums.end());
-            return ;
-        }
-        int num=nums[i-1];
-        int index=i;
-        int min=INT_MAX;
-        for(int j=i;j<nums.size();j++){
-            if((nums[j]-num)>0 && (nums[j]-num)<min){
-                //num=nums[j];
-                index=j;
+            if (i == 0)
+            {
+                sort(nums.begin(), nums.end());
+                return;
             }
+            int num = nums[i - 1];
+            int index = i;
+            int min = INT_MAX;
+            for (int j = i; j < nums.size(); j++)
+            {
+                if ((nums[j] - num) > 0 && (nums[j] - num) < min)
+                {
+                    index = j;
+                }
+            }
+            swap(nums[i - 1], nums[index]);
+            reverse(nums.begin() + i, nums.end());
         }
-        swap(nums[i-1], nums[index]);
-        reverse(nums.begin()+i, nums.end());
-    }
 };
