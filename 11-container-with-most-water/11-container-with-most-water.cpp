366f1ac44312bb4a09f71a337a918a1039a2abd3
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-05-29 15:42:07 +0530

    Time: 142 ms (30.26%), Space: 59.1 MB (38.97%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,54 +1,22 @@
 #include <bits/stdc++.h>
 
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-//         vector<pair<int, int>> left;
-//         vector<pair<int, int>> right(height.size());
-//         int n=height.size();
-//         if(n==1){
-//             return 0;
-//         }
-//         left.push_back({height[0], 1});
-//         right[n-1]={height[n-1], n};
-//         int leftmax=height[0];
-//         int index=0;
-//         for(int i=1;i<n;i++){
-//             if(leftmax>height[i]){
-//                 leftmax=height[i];
-//                 index=i+1;
-//             }
-            
-//             left.push_back({leftmax, index});
-//         }
-//         leftmax=height[n-1];
-//         index=n-1;
-  
-//         for(int i=n-2;i>=0;i--){
-//             if(leftmax>height[i]){
-//                 leftmax=height[i];
-//                 index=i+1;
-//             }
-//             right[i]={leftmax, index};
-//         }
-        
-//         int ans=INT_MIN;
-//         for(int i=0;i<n;i++){
-//             int h=min(right[i].first, left[i].first);
-//             int width=abs(right[i].second- left[i].second);
-//             ans=max(ans, h*width);
-//         }
-//         return ans;
-//     }
-        
-         int water = 0;
-    int i = 0, j = height.size() - 1;
-    while (i < j) {
-        int h = min(height[i], height[j]);
-        water = max(water, (j - i) * h);
-        while (height[i] <= h && i < j) i++;
-        while (height[j] <= h && i < j) j--;
-    }
-    return water;
-    }
+class Solution
+{
+    public:
+        int maxArea(vector<int> &height)
+        {
+
+           	int water = 0;
+           	int i = 0, j = height.size() - 1;
+           	while(i<j){
+                if(height[i]<=height[j]){
+                    water=max(water, (j-i)*height[i]);
+                    i++;
+                } else {
+                    water=max(water, (j-i)*height[j]);
+                    j--;
+                }
+            }
+           	return water;
+        }
 };
