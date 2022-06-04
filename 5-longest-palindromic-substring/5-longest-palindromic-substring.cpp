1b92ee5a3c1f7bdeceb2b5dba574ce756a4c8526
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-06-04 16:52:59 +0530

    Time: 1065 ms (8.85%), Space: 386.1 MB (9.45%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,38 +1,55 @@
-class Solution
-{
-    public:
-        string longestPalindrome(string s)
-        {
-            int n = s.size();
-            vector<vector < int>> dp(n + 1, vector<int> (n + 1));
-            int start = 0, end = 0;
-            int ans = 1;
-            for (int i = 0; i < n; i++)
-                dp[i][i] = 1;
-            for (int gap = 1; gap < n; gap++)
-            {
-                for (int i = 0; i < n - gap; i++)
-                {
-                    if (gap == 1)
-                    {
-                        if (s[i] == s[i + 1])
-                        {
-                            dp[i][i + 1] = 1;
-                            dp[i + 1][i] = 1;
-                            start = i, end = i + 1, ans = 2;
-                        }
-                    }
-                    int j = gap + i;
-                    if (s[i] == s[j] && dp[i + 1][j - 1])
-                    {
-                        dp[i][j] = 1;
-                        if (j - i + 1 > ans)
-                            ans = j - i + 1, start = i, end = j;
-                    }
-                    else
-                        dp[i][j] = 0;
-                }
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.length();
+        if(n==1){
+            return s;
+        } else if(n==2){
+            if(s[0]==s[1]){
+                return s;
+            } else {
+                return s.substr(0,1);
             }
-            return s.substr(start, end - start + 1);
         }
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
+        }
+        int ans=1;
+        string gg= s.substr(0,1);
+        for(int gap=1;gap<n;gap++){
+            if(gap==1){
+                for(int i=0;i<n-1;i++){
+                    if(s[i]==s[i+1]){
+                        dp[i][i+1]=2;
+                    } else {
+                         dp[i][i+1]=0;
+                    }
+                    if(dp[i][i+1]>ans){
+                        ans=max(ans, dp[i][i+1]);
+                        gg=s.substr(i, gap+1);
+                    }
+                   
+                }
+            } else {
+                 for(int i=0;i+gap<n;i++){
+                     if(s[i]==s[i+gap] && dp[i+1][i+gap-1]>0){
+                         dp[i][i+gap]=2+dp[i+1][i+gap-1];
+                         
+                     } else {
+                         dp[i][i+gap]=0;
+                     }
+                     if(dp[i][i+gap]>ans){
+                        ans=max(ans, dp[i][i+gap]);
+                        gg=s.substr(i, gap+1);
+                    }
+                     
+                 }
+            }
+            
+            
+        }
+        return gg;
+        
+    }
 };
