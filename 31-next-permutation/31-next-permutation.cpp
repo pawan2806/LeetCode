5dc503ba00e34e5f63ddb1f497b81cbacd8f55c5
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-07-30 22:12:23 +0530

    Time: 4 ms (76.85%), Space: 12.1 MB (74.59%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -17,15 +17,9 @@ class Solution
                 return;
             }
             int num = nums[i - 1];
-            int index = i;
-            int min = INT_MAX;
-            for (int j = i; j < nums.size(); j++)
-            {
-                if ((nums[j] - num) > 0 && (nums[j] - num) < min)
-                {
-                    
-                    index = j;
-                }
+            int index = nums.size()-1;
+            while(i-1<index && num>=nums[index]){
+                index--;
             }
             swap(nums[i - 1], nums[index]);
             reverse(nums.begin() + i, nums.end());
