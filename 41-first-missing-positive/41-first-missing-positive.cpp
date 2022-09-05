529002a1ef7bfa8fefc28420fb1696188e0a66ad
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2022-09-06 03:05:13 +0530

    Time: 108 ms (82.04%), Space: 31.4 MB (82.59%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -5,58 +5,31 @@ class Solution
         {
 
             auto i = 0;
-            int n=nums.size();
-            while (i < n)
+            int n = nums.size();
+            for (int i = 0; i < n; i++)
             {
-               	// cout << i << endl;
-                if (nums[i] == i + 1)
+                if (nums[i] <= 0 || nums[i] > n)
                 {
-                    i++;
-                }
-                else if (nums[i] <= 0 || nums[i] > n)
-                {
-                    nums[i++] = 0;
-                }
-                else if (nums[nums[i] - 1] == nums[i])
-                {
-                    nums[i++] = 0;
+                    continue;
                 }
                 else
                 {
-                    swap(nums[i], nums[nums[i] - 1]);
+                    int chair = nums[i] - 1;
+                    int element = nums[i];
+                    if (nums[chair] != element)
+                    {
+                        swap(nums[chair], nums[i--]);
+                    }
                 }
             }
-            for (auto i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
-                if (nums[i] == 0)
-                    return i + 1;
+                if (nums[i]!=i+1)
+                {
+                    return i+1;
+                }
+               
             }
-            return n + 1;
-
-           	//         int n=nums.size();
-           	//         int maxim=INT_MIN;
-           	//        	//bool npres=false;
-           	//         for(int i=0;i < n;i++){
-           	//             maxim=max(maxim, nums[i]);
-           	//         }
-           	//         for(int i=0;i < n;i++){
-           	//             if(nums[i]<=0){
-           	//                 nums[i]=maxim;
-           	//             }
-
-           	//         }
-           	//         for(int i=0;i < n;i++){
-           	//             if(abs(nums[i]) < n){
-           	//                 if(nums[abs(nums[i])]>=0)
-           	//                     nums[abs(nums[i])]*=(-1);
-           	//             } 
-           	//         }
-           	//         for(int i=1;i < n;i++){
-           	//             if(nums[i]>0){
-           	//                 return i;
-           	//             }
-           	//         }
-
-           	//         return maxim+1;
+            return n+1;
         }
 };
